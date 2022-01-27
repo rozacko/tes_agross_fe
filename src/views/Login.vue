7cf80c227ee4cadda9e0c3da766e463cf7f9bb56
esayo.dev <rozacko.rz@gmail.com>
2022-01-27 09:12:30 +0700

    update

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,129 +1,156 @@
+//login.vue
 <template>
-    <div class="login">
-        <div class="container" style="margin-top:100px">
-            <div class="row justify-content-center">
-                <div class="col-md-5">
-                    <div v-if="loginFailed" class="alert alert-danger">
-                        Email atau Password Anda salah.
-                    </div>
-                    <div class="card">
-                        <div class="card-body">
-                            LOGIN
-                            <hr>
-                            <form @submit.prevent="login">
-
-                                <div class="form-group">
-                                    <label>EMAIL</label>
-                                    <input type="email" class="form-control" v-model="user.email"
-                                        placeholder="Masukkan Email">
-                                    <div v-if="validation.email" class="mt-2 alert alert-danger">
-                                        Masukkan Email
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label>PASSWORD</label>
-                                    <input type="password" class="form-control" v-model="user.password"
-                                        placeholder="Masukkan Password">
-                                    <div v-if="validation.password" class="mt-2 alert alert-danger">
-                                        Masukkan Password
-                                    </div>
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">LOGIN</button>
-                            </form>
-                        </div>
-                    </div>
+  <div class="row justify-content-md-center">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-body">
+          <h1><b>Masuk Sekarang</b></h1>
+          <p>Masuk dan nikmati fitur kami</p>
+          <div v-if="loginFailed" class="alert alert-danger">
+            Email atau Password Anda salah.
+          </div>
+          <form @submit.prevent="login">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="name">Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    placeholder="Contoh : Contoh: bambang@gmail.com"
+                    v-model="user.email"
+                  />
+                  <div v-if="validation.email" class="mt-2 alert alert-danger">
+                    Masukkan Email
+                  </div>
                 </div>
+                <button type="submit" class="btn btn-success">
+                  Masuk Sekarang
+                </button>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    v-model="user.password"
+                    placeholder="Masukkan Password"
+                  />
+                  <div
+                    v-if="validation.password"
+                    class="mt-2 alert alert-danger"
+                  >
+                    Masukkan Password
+                  </div>
+                </div>
+              </div>
             </div>
+          </form>
         </div>
+      </div>
     </div>
+    <div class="col-md-12" style="padding-top: 50px; padding-botton: 150px">
+      <div>
+        <b><h2>Cerita Kerabat AGROS</h2></b>
+        <div class="row">
+          <div class="col-sm-6">
+            <img alt="Vue logo" src="../assets/4.png" />
+          </div>
+          <div class="col-sm-6">
+            <p class="card-text" style="margin: auto; padding: 100px 0">
+              Terinspirasi dari arah mata angin yang membawa pada satu
+              destinasi, Agros akan terus bergerak menciptakan pemerataan
+              ekonomi sehingga bisa menjadi penghubung para stakeholders dalam
+              aktivitas muatan berat, mulai dari shipper, transporter, driver,
+              mitra pemeliharan, seller dan buyer intermoda yang menjangkau
+              seluruh penjuru Nusantara.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios'
+import axios from 'axios';
 
-    export default {
-        name: 'Login',
+export default {
+  name: 'Login',
 
-        data() {
-            return {
-                //state loggedIn with localStorage
-                loggedIn: localStorage.getItem('loggedIn'),
-                //state token
-                token: localStorage.getItem('token'),
-                //state user
-                user: [],
-                //state validation
-                validation: [],
-                //state login failed
-                loginFailed: null
-            }
-        },
-        methods: {
+  data() {
+    return {
+      //state loggedIn with localStorage
+      loggedIn: localStorage.getItem('loggedIn'),
+      //state token
+      token: localStorage.getItem('token'),
+      //state user
+      user: [],
+      //state validation
+      validation: [],
+      //state login failed
+      loginFailed: null,
+    };
+  },
+  methods: {
+    login() {
+      if (this.user.email && this.user.password) {
+        axios
+          .get('http://localhost:8001/sanctum/csrf-cookie')
+          .then((response) => {
+            //debug cookie
+            console.log(response);
 
-            login() {
-                if (this.user.email && this.user.password) {
-                    axios.get('http://localhost:8001/sanctum/csrf-cookie')
-                        .then(response => {
+            axios
+              .post('http://localhost:8001/api/login', {
+                email: this.user.email,
+                password: this.user.password,
+              })
+              .then((res) => {
+                //debug user login
+                console.log(res);
 
-                            //debug cookie
-                            console.log(response)
+                if (res.data.success) {
+                  //set localStorage
+                  localStorage.setItem('loggedIn', 'true');
 
-                            axios.post('http://localhost:8001/api/login', {
-                                email: this.user.email,
-                                password: this.user.password
-                            }).then(res => {
+                  //set localStorage Token
+                  localStorage.setItem('token', res.data.token);
 
-                                //debug user login
-                                console.log(res)
+                  //change state
+                  this.loggedIn = true;
 
-                                if (res.data.success) {
-
-                                    //set localStorage
-                                    localStorage.setItem("loggedIn", "true")
-
-                                    //set localStorage Token
-                                    localStorage.setItem("token", res.data.token)
-
-                                    //change state
-                                    this.loggedIn = true
-
-                                    //redirect dashboard
-                                    return this.$router.push({ name: 'dashboard' })
-
-                                } else {
-
-                                    //set state login failed
-                                    this.loginFailed = true
-
-                                }
-
-                            }).catch(error => {
-                                console.log(error)
-                            })
-
-                        })
+                  //redirect dashboard
+                  return this.$router.push({ name: 'dashboard' });
+                } else {
+                  //set state login failed
+                  this.loginFailed = true;
                 }
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          });
+      }
 
-                this.validation = []
+      this.validation = [];
 
-                if (!this.user.email) {
-                    this.validation.email = true
-                }
+      if (!this.user.email) {
+        this.validation.email = true;
+      }
 
-                if (!this.user.password) {
-                    this.validation.password = true
-                }
+      if (!this.user.password) {
+        this.validation.password = true;
+      }
+    },
+  },
 
-            }
-        },
-
-        //check user already logged in
-        mounted() {
-            if (this.loggedIn) {
-                return this.$router.push({ name: 'dashboard' })
-            }
-        }
+  //check user already logged in
+  mounted() {
+    if (this.loggedIn) {
+      return this.$router.push({ name: 'App' });
     }
+  },
+};
 </script>
